8573ca4d45c9680a762bf25bf2aed69b4cec2866
coffmanjrp <coffmanjrp@gmail.com>
2021-08-23 00:12:16 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>We Love Programming!</h1>
+      <div className="container">
+        <label htmlFor="speed">Speed:</label>
+        <input
+          type="number"
+          name="speed"
+          id="speed"
+          value={1}
+          min="1"
+          max="5"
+          step="1"
+        />
+      </div>
     </div>
   );
 }
